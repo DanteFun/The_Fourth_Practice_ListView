d67c08a1ee612ca6eac46f22e9440a87e4d162be
DanteFun <1610671801@qq.com>
2018-04-17 15:22:29 +0800

    Initial commit

---
+++ app/src/main/java/com/fun/dante/four_listview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fun.dante.four_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
